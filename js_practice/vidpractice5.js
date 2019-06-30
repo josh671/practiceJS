72e15d9d6b3311e28778f6ef7174b5891e78fb9b
joshyb671 <j.bordallo671@outlook.com>
2019-06-30 10:55:13 -0700

    working with js objects and their key value pairs

--- js_practice/vidpractice5.js
+++ js_practice/vidpractice5.js
@@ -47,6 +47,58 @@ cc = (card) =>{
     return count + "" + holdbet; 
 } 
 
-cc(2); cc(3); cc(7); cc('k'); cc('a'); 
-//console.log(cc(10)) tells you to hold because 0 -1 
-1.47.23
+cc(2); cc(4); cc(7); cc('k'); cc('a'); 
+//console.log(cc(4))// tells you to bet because 3-2 = 1; 
+
+//BUILDING JAVASCRIPT OBJECTS
+//OBJECTS - use properties to access data; contained within curley braces
+
+let ourDog = { 
+    "name": "camper", 
+    "legs": 4, 
+    "tailes": 1,
+    "friends" : ["everything"], 
+}; 
+ 
+let myDog = { 
+    "name": "roket", 
+    "legs": 3, 
+    "tails": 2, 
+    "friends": [], 
+
+} 
+
+//ACCESSING OBJECT PROPERTIES WITH DOT (.) NOTATION; 
+let testObj = { 
+    "hat": "ballcap", 
+    "shirt": "jersey", 
+    "shoes": "cleats",
+};
+
+//finding out values of testObj using . notation; 
+let hatValue = testObj.hat; 
+//console.log(hatValue)// returns the value of hat within the object "testObj" 
+let shirtValue = testObj.shirt; 
+//console.log(shirtValue);// returns jersey
+let shoesValue = testObj.shoes; 
+//console.log(shoesValue); // returns value of shoes in object "testObject" 
+
+
+//ACCESSING OBJECT PROPERTIES WITH BRACKET NOTATION 
+let testObj2 = { 
+    "an entree": "hamburger", //bracket notation required when key has space 
+    "my side": "veggies",
+    "the drink": "water", 
+}; //finish creating object 
+
+let entreeValue = testObj2["an entree"]; //use brackets when key has space
+//console.log(entreeValue)// returns hamburger  
+let sideValue = testObj2["my side"]; 
+//console.log(sideValue); // returns veggies
+let drinkValue = testObj2["the drink"]; 
+console.log(drinkValue); // returns water;  
+
+//ACCESSING OBJECT PROPERTIES WITH VARIABLES 
+1.52.47 
+https://www.youtube.com/watch?v=PkZNo7MFNFg&t=1145s
+

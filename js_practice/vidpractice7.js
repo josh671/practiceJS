88d9283090f502e4eaa03c0d4892f6926211c3d0
joshyb671 <j.bordallo671@outlook.com>
2019-07-10 10:49:05 -0700

    adding vidpractice7 2.37.12

--- js_practice/vidpractice7.js
+++ js_practice/vidpractice7.js
@@ -25,8 +25,36 @@ let randomNumberBetween0and19 = Math.floor(Math.random()*20);
     ourRandomRange2 = (ourMin, ourMax) =>{ 
         return Math.floor(Math.random()*(ourMax - ourMin + 1))+ourMin; 
     }
-    console.log(ourRandomRange2(5,15)); 
+    //console.log(ourRandomRange2(5,15)); 
 
+    //USE THE PARSE INT FUNCTION
+convertToInteger =(str)=>{ //converts string into integer, returns NaN if cant convert
+    return parseInt(str);
+}
 
+//console.log('56');  
 
+//USE THE PARSEINT FUNCTION WITH A RADIX
+//radix specifies base of number in a string
+converttoInteger = (str) =>{ 
+    return parseInt(str, 2);
+}
+//console.log(converttoInteger('10056'))
 
+//USE THE CONDITIONAL (TERNARY) OPERATOR
+checkEqual = (a,b) =>{ 
+    return a === b ? true : false; // returns true if a ===b, false if a doesnt equal b 
+}
+
+//console.log(checkEqual(1,2)); returns false 
+
+//USE MULTIPLE CONDITIONAL (TERNARY) OPERATORS 
+//can be nested within eachother
+checkSign = (num) =>{ 
+    return num > 0 ? "positive" : num < 0 ? 'negative' : "zero"; 
+}
+//console.log(checkSign(0)); returns 0 ; 
+
+//DIFFERENCE BETWEEN THE 'VAR' AND 'LET' KEYWODS
+//let = keeps you from declairing a variable twice even if second use is in function
+2.37.14
